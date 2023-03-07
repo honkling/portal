b3307ffd0759d7ca7397a0f669d92a4552ca271b
honkling <untitledgeese@gmail.com>
2023-03-07 09:01:59 -0600

    Initial commit

---
+++ src/main/java/me/honkling/portal/Portal.java
@@ -0,0 +1,16 @@
+package me.honkling.portal;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Portal extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		
+	}
+
+	@Override
+	public void onDisable() {
+		// Plugin shutdown logic
+	}
+}
